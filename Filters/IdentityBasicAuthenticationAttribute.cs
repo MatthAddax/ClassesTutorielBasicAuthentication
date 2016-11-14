e5601d5e13cbfc647ed3246aaca9778bee94d920
Matthieu Meunier <matth.meun@hotmail.com>
2016-11-14 20:37:34 +0100

    first commit

---
+++ Filters/IdentityBasicAuthenticationAttribute.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using NomadModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using WebAPI.Exceptions;
+using WebAPI.Services;
+
+namespace WebAPI.Filters
+{
+    public class IdentityBasicAuthenticationAttribute : BasicAuthenticationAttribute
+    {
+        protected override async Task<IPrincipal> AuthenticateAsync(string userName, string password, CancellationToken cancellationToken)
+        {
+            UserManager<User> userManager = CreateUserManager();
+
+            cancellationToken.ThrowIfCancellationRequested(); // Unfortunately, UserManager doesn't support CancellationTokens.
+            var email = userName;
+            User user = await userManager.FindAsync(email, password);
+
+            if (user == null)
+            {
+                // No user with userName/password exists.
+                return null;
+            }
+			
+            // Create a ClaimsIdentity with all the claims for this user.
+            cancellationToken.ThrowIfCancellationRequested(); // Unfortunately, IClaimsIdenityFactory doesn't support CancellationTokens.
+            ClaimsIdentity identity = await userManager.ClaimsIdentityFactory.CreateAsync(userManager, user, "Basic");
+            
+            return new ClaimsPrincipal(identity);
+        }
+
+        private static UserManager<NomadModel.User> CreateUserManager()
+        {
+            //return new UserManager<NomadModel.User>(new UserStore<NomadModel.User>(new NomadModel.NomadContext()));
+            return new UserManager<NomadModel.User>(new UserStoreService()) { PasswordHasher = new PasswordStorage() };
+        }
+    }
+}
---
+++ Results/ForbiddenResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebAPI.Results
+{
+    public class ForbiddenResult : IHttpActionResult
+    {
+        private readonly HttpRequestMessage _request;
+        private readonly string _reason;
+
+        public ForbiddenResult(string reason, HttpRequestMessage request)
+        {
+            _request = request;
+            _reason = reason;
+        }
+
+        public ForbiddenResult(HttpRequestMessage request)
+        {
+            _request = request;
+            _reason = "Forbidden";
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = _request.CreateResponse(HttpStatusCode.Forbidden, "Forbidden");
+            response.ReasonPhrase = _reason;
+            return Task.FromResult(response);
+        }
+    }
+}
